2dff305dd017af49dd49ab0c578dd56addc13421
Nils Weiß <n125.we155@googlemail.com>
2016-05-13 16:07:12 +0200

    cleanup TIM

--- project/config/Tim_config.h
+++ project/config/Tim_config.h
@@ -24,6 +24,10 @@ enum Description {
     __ENUM__SIZE
 };
 
+static const uint32_t HALFBRIDGE_PERIODE = 4000;
+static const uint32_t HALL_SENSOR_PRESCALER = 179;
+static const uint32_t BUZZER_PWM_PERIODE = 1000;
+
 #else
 #ifndef SOURCES_PMD_TIM_CONFIG_CONTAINER_H_
 #define SOURCES_PMD_TIM_CONFIG_CONTAINER_H_
