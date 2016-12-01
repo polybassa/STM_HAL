8f0be552689b849c5d20e43ad4982d168a567b2b
Nils Weiß <n125.we155@googlemail.com>
2016-12-01 07:38:54 +0100

    port vesc motorController uart library

---
+++ sources/app/VescMotorController.cpp
@@ -0,0 +1,139 @@
+/* Copyright (C) 2016  Nils Weiss
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include "VescMotorController.h"
+#include "trace.h"
+#include <cmath>
+#include "RealTimeDebugInterface.h"
+#include "vesc_bldc_interface_uart.h"
+#include "vesc_bldc_interface.h"
+#include <functional>
+
+using app::VescMotorController;
+
+extern dev::RealTimeDebugInterface* g_RTTerminal;
+
+static const int __attribute__((unused)) g_DebugZones = ZONE_ERROR | ZONE_WARNING | ZONE_VERBOSE | ZONE_INFO;
+
+constexpr std::chrono::milliseconds VescMotorController::controllerInterval;
+
+static VescMotorController* internalMotorControllerPointer = nullptr;
+
+static void app::send_packet_callback(unsigned char* data, unsigned int len)
+{
+    if (internalMotorControllerPointer) {
+        internalMotorControllerPointer->send_packet(data, len);
+    }
+}
+
+static void app::bldc_val_received(void* valPtr)
+{
+    if (valPtr) {
+        mc_values* val = reinterpret_cast<mc_values*>(valPtr);
+
+        //UNCOMMENT for TRACING
+        //	g_RTTerminal->printf("Input voltage: %5d mV\r\n",       static_cast<size_t>(val->v_in * 1000));
+        //	g_RTTerminal->printf("Temp:          %5d mdegC\r\n",static_cast<size_t>(val->temp_pcb* 1000));
+        //	g_RTTerminal->printf("Current motor: %5d mA\r\n",       static_cast<size_t>(val->current_motor* 1000));
+        //	g_RTTerminal->printf("Current in:    %5d mA\r\n",       static_cast<size_t>(val->current_in* 1000));
+        //	g_RTTerminal->printf("RPM:           %5d RPM\r\n",      static_cast<size_t>(val->rpm* 1000));
+        //	g_RTTerminal->printf("Duty cycle:    %5d %%\r\n",       static_cast<size_t>(val->duty_now * 100.0));
+        //	g_RTTerminal->printf("Ah Drawn:      %5d mAh\r\n",      static_cast<size_t>(val->amp_hours* 1000));
+        //	g_RTTerminal->printf("Ah Regen:      %5d mAh\r\n",      static_cast<size_t>(val->amp_hours_charged* 1000));
+        //	g_RTTerminal->printf("Wh Drawn:      %5d mWh\r\n",      static_cast<size_t>(val->watt_hours* 1000));
+        //	g_RTTerminal->printf("Wh Regen:      %5d mWh\r\n",      static_cast<size_t>(val->watt_hours_charged* 1000));
+        //	g_RTTerminal->printf("Tacho:         %i counts\r\n", val->tachometer);
+        //	g_RTTerminal->printf("Tacho ABS:     %i counts\r\n", val->tachometer_abs);
+        //	g_RTTerminal->printf("Fault Code:    %s\r\n", bldc_interface_fault_to_string(val->fault_code));
+
+        if (internalMotorControllerPointer) {
+            internalMotorControllerPointer->mCurrentRPS = val->rpm / 60;
+        }
+    }
+}
+
+VescMotorController::VescMotorController(const hal::Usart& interface) :
+    os::DeepSleepModule(),
+    mMotorControllerTask("VescMotorControl",
+                         VescMotorController::STACKSIZE,
+                         os::Task::Priority::LOW,
+                         [this](const bool& join)
+                         {
+                             motorControllerTaskFunction(join);
+                         }),
+    mSetTorqueQueue(),
+    mInterface(interface)
+{
+    setTorque(0.00001);
+    internalMotorControllerPointer = this;
+    ::bldc_interface_uart_init(send_packet_callback);
+    ::bldc_interface_set_rx_value_func(reinterpret_cast<void (*)(mc_values*)>(bldc_val_received));
+}
+
+void VescMotorController::enterDeepSleep(void)
+{
+    mMotorControllerTask.join();
+}
+
+void VescMotorController::exitDeepSleep(void)
+{
+    mMotorControllerTask.start();
+}
+
+void VescMotorController::send_packet(unsigned char* data, unsigned int len)
+{
+    mInterface.send(data, len);
+}
+
+void VescMotorController::motorControllerTaskFunction(const bool& join)
+{
+    // if update of RPS Value is to slow, use a smaller value here!
+    const size_t receiveValuesPeriode = 1000;
+
+    size_t executionCounter = 0;
+    do {
+        float newSetTorque;
+        if (mSetTorqueQueue.receive(newSetTorque, 0)) {
+            constexpr const float CPHI = 0.0065;
+            bldc_interface_set_current(newSetTorque / CPHI);
+        }
+
+        // process received data
+        const unsigned int arrayLength = 128;
+        unsigned char dataArray[arrayLength];
+
+        const size_t receivedBytes = mInterface.receiveAvailableData(dataArray, arrayLength);
+
+        for (size_t i = 0; i < receivedBytes; i++) {
+            ::bldc_interface_uart_process_byte(dataArray[i]);
+        }
+
+        // trigger get values every "receiveValuesPeriode" cycle of this loop
+        if (executionCounter == 0) {
+            bldc_interface_get_values();
+        }
+        executionCounter = ++executionCounter % receiveValuesPeriode;
+    } while (!join);
+}
+
+void VescMotorController::setTorque(const float setValue)
+{
+    mSetTorqueQueue.overwrite(setValue);
+}
+
+float VescMotorController::getCurrentRPS(void) const
+{
+    return mCurrentRPS;
+}
---
+++ sources/app/VescMotorController.h
@@ -0,0 +1,68 @@
+/* Copyright (C) 2016  Nils Weiss
+ # *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef SOURCES_PMD_VESCMOTORCONTROLLER_H_
+#define SOURCES_PMD_VESCMOTORCONTROLLER_H_
+
+#include "interface_MotorController.h"
+#include "TaskInterruptable.h"
+#include "DeepSleepInterface.h"
+#include "os_Queue.h"
+#include "Usart.h"
+#include <limits>
+
+namespace app
+{
+static void send_packet_callback(unsigned char*, unsigned int);
+static void bldc_val_received(void* valPtr);
+
+class VescMotorController final :
+    private os::DeepSleepModule, public interface::MotorController
+{
+    virtual void enterDeepSleep(void) override;
+    virtual void exitDeepSleep(void) override;
+
+    static constexpr uint32_t STACKSIZE = 1024;
+
+    os::TaskInterruptable mMotorControllerTask;
+    os::Queue<float, 1> mSetTorqueQueue;
+    const hal::Usart& mInterface;
+    float mCurrentRPS;
+
+    static constexpr std::chrono::milliseconds controllerInterval = std::chrono::milliseconds(2);
+
+    void motorControllerTaskFunction(const bool&);
+    void send_packet(unsigned char* data, unsigned int len);
+
+public:
+    VescMotorController(const hal::Usart & interface);
+
+    VescMotorController(const VescMotorController &) = delete;
+    VescMotorController(VescMotorController &&) = delete;
+    VescMotorController& operator=(const VescMotorController&) = delete;
+    VescMotorController& operator=(VescMotorController &&) = delete;
+
+    virtual void setTorque(const float) override;
+    virtual float getCurrentRPS(void) const override;
+
+#ifdef UNITTEST
+    void triggerTaskExecution(void) { this->motorControllerTaskFunction(true); }
+#endif
+    friend void send_packet_callback(unsigned char*, unsigned int);
+    friend void bldc_val_received(void* valPtr);
+};
+}
+
+#endif /* SOURCES_PMD_VESCMOTORCONTROLLER_H_ */
