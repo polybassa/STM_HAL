45fc6feb280cd3c914441f9de5e5619ae4525014
Nils Weiss <nils@we155.de>
2018-01-26 17:28:10 +0000

    add 12 MHz Oscillator

--- libraries/CMSIS/Device/ST/STM32F10x/system_stm32f10x.c
+++ libraries/CMSIS/Device/ST/STM32F10x/system_stm32f10x.c
@@ -1053,7 +1053,13 @@ static void SetSysClockTo72(void)
     /*  PLL configuration: PLLCLK = HSE * 9 = 72 MHz */
     RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE |
                                         RCC_CFGR_PLLMULL));
+#if (HSE_VALUE == 12000000)
+    RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_HSE | RCC_CFGR_PLLMULL6);
+#pragma message "External Oscillator Clock 12MHz"
+#else
     RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_HSE | RCC_CFGR_PLLMULL9);
+#pragma message "External Oscillator Clock 8MHz"
+#endif
 #endif /* STM32F10X_CL */
 
     /* Enable PLL */
