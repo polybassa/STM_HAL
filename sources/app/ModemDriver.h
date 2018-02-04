4bdcd3c06943ca9afd82260bcd51d4c85e88bb51
Nils Weiss <nils@we155.de>
2018-02-04 16:23:29 +0100

    change copyright

--- sources/app/ModemDriver.h
+++ sources/app/ModemDriver.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2015  Nils Weiss, Daniel Tatzel, Markus Wildgruber
+/* Copyright (C) 2015  Nils Weiss
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
