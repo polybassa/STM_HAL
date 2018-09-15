6803702c7df294ee9ffbb5273345db2dd5b19d18
Nils Weiss <nils@we155.de>
2018-09-15 15:36:23 +0200

    add tunnel project

---
+++ project_tunnel/config/Gpio_config.h
@@ -0,0 +1,110 @@
+/* Copyright (C) 2015  Nils Weiss
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef SOURCES_PMD_GPIO_CONFIG_DESCRIPTION_H_
+#define SOURCES_PMD_GPIO_CONFIG_DESCRIPTION_H_
+
+enum Description {
+    // ===PORTA===
+    USART2_TX,
+    USART2_RX,
+    USART1_TX,
+    USART1_RX,
+    SWDIO,
+    SWCLK,
+    // ===PORTB===
+    SECCO_PWR,
+    USART3_TX,
+    USART3_RX,
+    // ===PORTC===
+    MODEM_RESET,
+    MODEM_POWER,
+    MODEM_SUPPLY,
+    // ===PORTD===
+    // ===PORTE===
+    // ===PORTF===
+    // ===PORTG===
+    __ENUM__SIZE
+};
+
+#else
+#ifndef SOURCES_PMD_GPIO_CONFIG_CONTAINER_H_
+#define SOURCES_PMD_GPIO_CONFIG_CONTAINER_H_
+
+static constexpr const std::array<const Gpio, Gpio::__ENUM__SIZE + 1> Container =
+{ {
+      // ===================PORTA=================
+      Gpio(Gpio::USART2_TX,
+           GPIOA_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_2, GPIO_Speed_50MHz, GPIO_Mode_AF_PP},
+           GPIO_PinSource2),
+      Gpio(Gpio::USART2_RX,
+           GPIOA_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_3, GPIO_Speed_50MHz, GPIO_Mode_IN_FLOATING},
+           GPIO_PinSource3),
+      Gpio(Gpio::USART1_TX,
+           GPIOA_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_9, GPIO_Speed_50MHz, GPIO_Mode_AF_PP},
+           GPIO_PinSource9),
+      Gpio(Gpio::USART1_RX,
+           GPIOA_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_10, GPIO_Speed_50MHz, GPIO_Mode_IN_FLOATING},
+           GPIO_PinSource10),
+      Gpio(Gpio::SWDIO,
+           GPIOA_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_13, GPIO_Speed_50MHz, GPIO_Mode_AF_PP},
+           GPIO_PinSource13),
+      Gpio(Gpio::SWCLK,
+           GPIOA_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_14, GPIO_Speed_50MHz, GPIO_Mode_AF_PP},
+           GPIO_PinSource14),
+      // ===================PORTB=================
+      Gpio(Gpio::SECCO_PWR,
+           GPIOB_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_0, GPIO_Speed_50MHz, GPIO_Mode_Out_PP}),
+      Gpio(Gpio::USART3_TX,
+           GPIOB_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_10, GPIO_Speed_50MHz, GPIO_Mode_AF_PP},
+           GPIO_PinSource10),
+      Gpio(Gpio::USART3_RX,
+           GPIOB_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_11, GPIO_Speed_50MHz, GPIO_Mode_IN_FLOATING},
+           GPIO_PinSource11),
+      // ===================PORTC=================
+      Gpio(Gpio::MODEM_RESET,
+           GPIOC_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_7, GPIO_Speed_50MHz, GPIO_Mode_Out_PP}),
+      Gpio(Gpio::MODEM_POWER,
+           GPIOC_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_8, GPIO_Speed_50MHz, GPIO_Mode_Out_PP}),
+      Gpio(Gpio::MODEM_SUPPLY,
+           GPIOC_BASE,
+           GPIO_InitTypeDef {GPIO_Pin_9, GPIO_Speed_50MHz, GPIO_Mode_Out_PP}),
+      // ===================PORTD=================
+      // ===================PORTE=================
+      // ===================PORTF=================
+      // ===================PORTG=================
+      Gpio(Gpio::__ENUM__SIZE,
+           GPIOD_BASE,
+           GPIO_InitTypeDef { GPIO_Pin_14, GPIO_Speed_50MHz, GPIO_Mode_AF_PP })
+  } };
+
+static constexpr const std::array<const uint32_t, 1> RemappingContainer =
+{{
+     GPIO_Remap_SWJ_JTAGDisable
+ }};
+
+#endif /* SOURCES_PMD_GPIO_CONFIG_CONTAINER_H_ */
+#endif /* SOURCES_PMD_GPIO_CONFIG_DESCRIPTION_H_ */
---
+++ project_tunnel/main.cpp
@@ -0,0 +1,81 @@
+/* Copyright (C) 2015  Nils Weiss
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+/* GENERAL INCLUDES */
+#include <string>
+
+#include "os_Task.h"
+#include "cpp_overrides.h"
+#include "trace.h"
+
+/* OS LAYER INCLUDES */
+#include "hal_Factory.h"
+#include "Gpio.h"
+#include "Usart.h"
+#include "Dma.h"
+#include "UsartWithDma.h"
+
+/* DEV LAYER INLCUDES */
+
+/* VIRT LAYER INCLUDES */
+
+/* COM LAYER INCLUDES */
+
+/* APP LAYER INLCUDES */
+#include "ModemDriver.h"
+#include "CanController.h"
+#include "CommandMultiplexer.h"
+#include "DemoExecuter.h"
+
+/* GLOBAL VARIABLES */
+static const int __attribute__((used)) g_DebugZones = ZONE_ERROR | ZONE_WARNING |
+                                                      ZONE_VERBOSE | ZONE_INFO;
+
+extern char _version_start;
+extern char _version_end;
+const std::string VERSION(&_version_start, (&_version_end - &_version_start));
+
+int main(void)
+{
+    hal::initFactory<hal::Factory<hal::Gpio> >();
+    hal::initFactory<hal::Factory<hal::Usart> >();
+    hal::initFactory<hal::Factory<hal::Dma> >();
+    hal::initFactory<hal::Factory<hal::UsartWithDma> >();
+
+    TraceInit();
+    Trace(ZONE_INFO, "Version: %s \r\n", VERSION.c_str());
+
+    auto modem = new app::ModemDriver(hal::Factory<hal::UsartWithDma>::get<hal::Usart::
+                                                                           MODEM_COM>(),
+                                      hal::Factory<hal::Gpio>::get<hal::Gpio::MODEM_RESET>(),
+                                      hal::Factory<hal::Gpio>::get<hal::Gpio::MODEM_POWER>(),
+                                      hal::Factory<hal::Gpio>::get<hal::Gpio::MODEM_SUPPLY>());
+
+    auto can = new app::CanController(hal::Factory<hal::UsartWithDma>::get<hal::Usart::SECCO_COM>(),
+                                      hal::Factory<hal::Gpio>::get<hal::Gpio::SECCO_PWR>(),
+                                      hal::Factory<hal::Gpio>::getAlternateFunctionGpio<hal::Gpio::USART2_TX>());
+
+    auto demo = new app::DemoExecuter(*can);
+    auto mux = new app::CommandMultiplexer(*modem, *can, *demo);
+
+    os::Task::startScheduler();
+
+    while (1) {}
+}
+
+void assert_failed(uint8_t* file, uint32_t line)
+{
+    Trace(ZONE_ERROR, "ASSERT FAILED: %s:%u", file, line);
+}
