01279c24a838044e9ba8b615916e19b218a6dc42
Denis Petri <Denis-Petri@web.de>
2018-11-21 17:40:17 +0100

    First steps with CAN on BluePill

---
+++ project_bluepill/apps/TestCan.cpp
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+ * Copyright (c) 2014-2018 Nils Weiss
+ */
+
+#include "TestCan.h"
+#include "Can.h"
+#include "trace.h"
+#include <cstring>
+
+static const int __attribute__((unused)) g_DebugZones = ZONE_ERROR | ZONE_WARNING | ZONE_VERBOSE | ZONE_INFO;
+
+const os::TaskEndless canTest("Can_Test",
+                              1024, os::Task::Priority::HIGH, [](const bool&){
+                              constexpr const hal::Can& can = hal::Factory<hal::Can>::get<hal::Can::MAINCAN>();
+
+                              Trace(ZONE_INFO, "Hallo MainCan\r\n");
+                              while (true) {
+                                  os::ThisTask::sleep(std::chrono::milliseconds(300));
+
+                                  CanTxMsg msg;
+                                  msg.StdId = 0x700;
+                                  msg.IDE = 0;
+                                  msg.RTR = 0;
+                                  msg.DLC = 5;
+                                  msg.Data[0] = 'H';
+                                  msg.Data[1] = 'e';
+                                  msg.Data[2] = 'l';
+                                  msg.Data[3] = 'l';
+                                  msg.Data[4] = 'o';
+
+                                  can.send(msg);
+
+                                  os::ThisTask::sleep(std::chrono::milliseconds(300));
+                                  Trace(ZONE_INFO, "pending %d\r\n", can.messagePending());
+
+                                  CanRxMsg rxmsg;
+                                  std::memset(&rxmsg, 0, sizeof(rxmsg));
+
+                                  auto ret = can.receive(rxmsg);
+
+                                  os::ThisTask::sleep(std::chrono::milliseconds(300));
+                                  Trace(ZONE_INFO, "pending %d\r\n", can.messagePending());
+                                  Trace(ZONE_INFO, "received %d\r\n", ret);
+                                  Trace(ZONE_INFO, "Data %s\r\n", rxmsg.Data);
+                              }
+    });
