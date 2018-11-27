62275acf64beb1133fcbb052220c749267dd5d34
Henning Mende <github@skotax.de>
2018-11-27 13:34:20 +0100

    remove unecessary cmath import

--- sources/hal_stm32f4xx/Adc.cpp
+++ sources/hal_stm32f4xx/Adc.cpp
@@ -13,7 +13,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
-#include <cmath>
 #include "stm32f4xx_misc.h"
 #include "trace.h"
 #include "LockGuard.h"
