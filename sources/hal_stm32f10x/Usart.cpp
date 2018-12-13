bd8c27ed5c372495145d6cf683036bd48bee8457
HeMe2 <github@skotax.de>
2018-12-13 08:40:10 +0100

    fix disableNonBlockingReceive: unset callback for stm32f1 and unset the RIGHT callback for stm32f3 (#43)

--- sources/hal_stm32f10x/Usart.cpp
+++ sources/hal_stm32f10x/Usart.cpp
@@ -122,6 +122,8 @@ void Usart::enableNonBlockingReceive(std::function<void(uint8_t)> callback) cons
 
 void Usart::disableNonBlockingReceive(void) const
 {
+    ReceiveInterruptCallbacks[mDescription] = nullptr;
+
     USART_ITConfig(reinterpret_cast<USART_TypeDef*>(mPeripherie), USART_IT_RXNE, DISABLE);
 }
 
@@ -165,7 +167,7 @@ size_t Usart::receive(uint8_t* const data, const size_t length) const
     size_t bytesReceived = 0;
     while (bytesReceived < length) {
         if (this->isReadyToReceive()) {
-            data[bytesReceived] = (uint8_t)this->receive();
+            data[bytesReceived] = (uint8_t) this->receive();
             bytesReceived++;
         }
     }
