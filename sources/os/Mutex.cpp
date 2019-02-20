05590c636e8b7f560d27e74c41017bf8ea33049b
Nils Weiss <nils@we155.de>
2019-02-20 12:57:05 +0100

    uncrustify changes

--- sources/os/Mutex.cpp
+++ sources/os/Mutex.cpp
@@ -11,13 +11,13 @@ Mutex::Mutex(void) :
     mMutexHandle(xSemaphoreCreateMutex())
 {}
 
-Mutex::Mutex(Mutex && rhs) :
+Mutex::Mutex(Mutex&& rhs) :
     mMutexHandle(rhs.mMutexHandle)
 {
     rhs.mMutexHandle = nullptr;
 }
 
-Mutex& Mutex::operator=(Mutex && rhs)
+Mutex& Mutex::operator=(Mutex&& rhs)
 {
     mMutexHandle = rhs.mMutexHandle;
     rhs.mMutexHandle = nullptr;
